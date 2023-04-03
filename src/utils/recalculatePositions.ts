ef4696266cfa2cb7e94da51700358c3d2ccecade
Andrii Bubnov <andrii.bubnov@cyrebro.io>
2023-04-03 12:25:38 +0200

    Fix recalculation bugs

--- src/utils/recalculatePositions.ts
+++ src/utils/recalculatePositions.ts
@@ -1,21 +1,91 @@
 import { Layout, Location } from '../types';
 
-export const recalculatePositions = (state: Layout, start: string, end: string): Layout => {
+const prepare = (state: Layout, start: string, end: string) => {
 	const { width: startWidth, height: startHeight, row: startRow, column: startColumn } = state[start];
 	const { width: endWidth, height: endHeight, row: endRow, column: endColumn } = state[end];
 
 	const isSameRow = startRow === endRow;
 	const isSameColumn = startColumn === endColumn;
 
-	const [main, cross] = isSameRow ? [Location.ROW, Location.COLUMN] : [Location.COLUMN, Location.ROW];
+	const [main, cross] = startColumn !== endColumn ? [Location.ROW, Location.COLUMN] : [Location.COLUMN, Location.ROW];
 
-	const [size, startSize, endSize, crossSize] = isSameRow
-		? [Location.WIDTH, startWidth, endWidth, Location.HEIGHT]
-		: [Location.HEIGHT, startHeight, endHeight, Location.WIDTH];
+	const [size, startSize, endSize, crossSize] =
+		startColumn !== endColumn
+			? [Location.WIDTH, startWidth, endWidth, Location.HEIGHT]
+			: [Location.HEIGHT, startHeight, endHeight, Location.WIDTH];
 
 	const { [start]: startElement, [end]: endElement } = state;
+	return {
+		startWidth,
+		startHeight,
+		startColumn,
+		endWidth,
+		endHeight,
+		endRow,
+		endColumn,
+		isSameRow,
+		isSameColumn,
+		main,
+		cross,
+		size,
+		startSize,
+		endSize,
+		crossSize,
+		startElement,
+		endElement,
+	};
+};
 
-	if (startElement[crossSize] < endElement[crossSize] || startElement[size] < endElement[size]) return state;
+const simple = (state: Layout, start: string, end: string): Layout => {
+	const { main, cross, size, startSize, endSize, startElement, endElement } = prepare(state, start, end);
+	const keys = Object.keys(state) as Array<keyof typeof state>;
+
+	const containerElements = keys.filter(el => state[el][main] === state[end][main]);
+	const fromRight = state[start][cross] > state[end][cross];
+	if (fromRight)
+		return {
+			...state,
+			...containerElements.reduce((acc, el) => {
+				const { [el]: element } = state;
+				if (el === start) return { ...acc, [start]: { ...endElement, [size]: startSize } };
+				if (el === end) {
+					return {
+						...acc,
+						[end]: {
+							...startElement,
+							[cross]: startElement[cross] + startSize - endSize,
+							[size]: endSize,
+						},
+					};
+				}
+				if (element[cross] < endElement[cross] || element[cross] > startElement[cross]) {
+					return { ...acc, [el]: state[el] };
+				}
+				return { ...acc, [el]: { ...state[el], [cross]: element[cross] + startSize - endSize } };
+			}, {} as Layout),
+		};
+	return simple(state, end, start);
+};
+
+export const recalculatePositions = (state: Layout, start: string, end: string): Layout => {
+	const {
+		startWidth,
+		startHeight,
+		endWidth,
+		endHeight,
+		endRow,
+		endColumn,
+		isSameRow,
+		isSameColumn,
+		main,
+		cross,
+		size,
+		crossSize,
+		startElement,
+		endElement,
+	} = prepare(state, start, end);
+
+	if (startElement[crossSize] < endElement[crossSize]) return state;
 
 	const keys = Object.keys(state) as Array<keyof typeof state>;
 
@@ -26,6 +96,7 @@ export const recalculatePositions = (state: Layout, start: string, end: string):
 		const target = keys
 			.filter(
 				key =>
+					key !== start &&
 					state[key][Location.COLUMN] >= endColumn &&
 					state[key][Location.ROW] >= endRow &&
 					state[key][Location.COLUMN] <= endColumn + startWidth - endWidth &&
@@ -36,7 +107,8 @@ export const recalculatePositions = (state: Layout, start: string, end: string):
 			(acc, cur) => acc + state[cur][Location.WIDTH] * state[cur][Location.HEIGHT],
 			0
 		);
-		return [target, targetSquare >= startSquare];
+		const allowedSingle = (target.length === 1 && isSameColumn) || (target.length === 1 && isSameRow);
+		return [target, allowedSingle || targetSquare === startSquare];
 	};
 
 	const diagonalNotEqual = () => {
@@ -60,8 +132,6 @@ export const recalculatePositions = (state: Layout, start: string, end: string):
 	};
 
 	const perpendicular = () => {
-		console.log('line 64');
-
 		const [target, allowed] = getTarget();
 
 		if (!allowed) return state;
@@ -82,41 +152,11 @@ export const recalculatePositions = (state: Layout, start: string, end: string):
 		};
 	};
 
-	const fromRightSimple = () => {
-		return {
-			...state,
-			...containerElements.reduce((acc, el) => {
-				const { [el]: element } = state;
-				if (el === start) return { ...acc, [start]: { ...endElement, [size]: startSize } };
-				if (el === end) {
-					return {
-						...acc,
-						[end]: {
-							...startElement,
-							[cross]: startElement[cross] + startSize - endSize,
-							[size]: endSize,
-						},
-					};
-				}
-				if (element[cross] < endElement[cross] || element[cross] > startElement[cross]) {
-					return { ...acc, [el]: state[el] };
-				}
-				return { ...acc, [el]: { ...state[el], [cross]: element[cross] + startSize - endSize } };
-			}, {} as Layout),
-		};
-	};
-
 	if (startElement[size] > endElement[size] && startElement[main] !== endElement[main]) return diagonalNotEqual();
 
 	if (startElement[crossSize] > endElement[crossSize]) return perpendicular();
 
-	if (!isSameRow && !isSameColumn) return diagonalEqual();
+	if (!isSameRow && !isSameColumn && startWidth === endWidth && startHeight === endHeight) return diagonalEqual();
 
-	const fromRight = state[start][cross] > state[end][cross];
-
-	const containerElements = keys.filter(el => state[el][main] === state[end][main]);
-
-	if (fromRight) return fromRightSimple();
-
-	return recalculatePositions(state, end, start);
+	return simple(state, end, start);
 };
