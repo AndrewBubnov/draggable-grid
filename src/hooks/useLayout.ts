95a23963fa354741fe1cd2761aa2fa694321174f
Andrii Bubnov <andrii.bubnov@cyrebro.io>
2023-04-04 13:50:08 +0200

    Refactor

--- src/hooks/useLayout.ts
+++ src/hooks/useLayout.ts
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useRef, useState } from 'react';
 import { Layout, DragStatus, Tiles } from '../types';
 import { getInitialChildrenStyle } from '../utils/getInitialChildrenStyle';
-import { AUTO, GAP, COLUMN_SPAN, TEMPLATE_COLUMNS, TEMPLATE_ROWS, ROW_SPAN } from '../constants';
+import { GAP, COLUMN_SPAN, TEMPLATE_COLUMNS, TEMPLATE_ROWS, ROW_SPAN } from '../constants';
 import { recalculatePositions } from '../utils/recalculatePositions';
 import { getComputedParam } from '../utils/getComputedParam';
 
@@ -27,7 +27,7 @@ export const useLayout = () => {
 				height: getComputedParam(computedHeight),
 			};
 		});
-		const initLayout = getInitialChildrenStyle(childCoords) as Layout;
+		const initLayout = getInitialChildrenStyle(childCoords);
 		startLayout.current = initLayout;
 		setLayout(initLayout);
 	}, []);
--- src/utils/getInitialChildrenStyle.ts
+++ src/utils/getInitialChildrenStyle.ts
@@ -1,35 +1,31 @@
-import { GetInitialStyleProp, TempLayout } from '../types';
+import { GetInitialStyleProp, Layout, TempLayout } from '../types';
 
 export const getInitialChildrenStyle = (array: GetInitialStyleProp[]) => {
 	const sortedColumns = array.sort((a, b) => a.coords.x - b.coords.x);
+
 	let currentX = sortedColumns[0].coords.x;
 	let currentGridColumn = 1;
-	let style: TempLayout = {};
-	sortedColumns.forEach(col => {
-		if (col.coords.x > currentX) {
-			currentX = col.coords.x;
+
+	const columnsOnly = sortedColumns.reduce((acc, cur) => {
+		if (cur.coords.x > currentX) {
+			currentX = cur.coords.x;
 			currentGridColumn = currentGridColumn + 1;
 		}
-		style = {
-			...style,
-			[col.id]: { column: currentGridColumn, width: col.width, height: col.height },
-		};
-	});
+		acc[cur.id] = { column: currentGridColumn, width: cur.width, height: cur.height };
+		return acc;
+	}, {} as TempLayout);
+
 	const sortedRaws = array.sort((a, b) => a.coords.y - b.coords.y);
+
 	let currentY = sortedColumns[0].coords.y;
 	let currentGridRow = 1;
-	sortedRaws.forEach(raw => {
-		if (raw.coords.y > currentY) {
-			currentY = raw.coords.y;
+
+	return sortedRaws.reduce((acc, cur) => {
+		if (cur.coords.y > currentY) {
+			currentY = cur.coords.y;
 			currentGridRow = currentGridRow + 1;
 		}
-		style = {
-			...style,
-			[raw.id]: {
-				...style[raw.id],
-				row: currentGridRow,
-			},
-		};
-	});
-	return style;
+		acc[cur.id] = { ...columnsOnly[cur.id], row: currentGridRow };
+		return acc;
+	}, {} as Layout);
 };
